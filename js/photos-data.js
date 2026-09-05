c1631712cf3c0d35ae7917c70dc7c36dded70a94
Denise Malan <denise.malan@dataiku.com>
2026-09-04 20:21:11 -0500

    Update photo source for Whooping crane

--- js/photos-data.js
+++ js/photos-data.js
@@ -41,7 +41,7 @@ const FILTERS = {
 const PHOTOS = [
   {
   number: "001",
-  src: "images/birds/_DSC9924.jpg",
+  src: "images/birds/_DSC9925-Enhanced-NR",
   alt: "Whooping crane standing in marshy field.",
   species: "Whooping crane",
   scientific: "Grus americana",
